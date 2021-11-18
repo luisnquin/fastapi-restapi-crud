94b53cc507bf693a7eef66efc4cbf9b3d07eb731
Luis Quinones <lpaandres2020@gmail.com>
2021-11-18 07:54:38 -0500

    Mod: index.html(nav link 'Update' removed), JS(New: divs, links(Update, delete). Removed: hr label.), SCSS(styles corresponding to the new JS code)

--- src/website/js/main.js
+++ src/website/js/main.js
@@ -3,6 +3,7 @@
 window.onload = function() {
     const content = document.getElementById('content');
 
+
     fetch('http://127.0.0.1:8000/movies', {})
     .then(response => response.json())
     .then(data => {
@@ -12,25 +13,47 @@ window.onload = function() {
 
 
 function appendContent(movie, context) {
-    let contentItem = document.createElement("div")
+    let contentItem = document.createElement("div");
+    let contentItemDiv = document.createElement("div");
+
+    let contentItemDivButtons = document.createElement("div");
+    let updateButton = document.createElement("a");
+    let deleteButton = document.createElement("a");
+
     contentItem.setAttribute("class", "content-item");
+    contentItem.setAttribute("id", `${movie.id}`);
     
-    let contentItemParagraphs = []
+    let contentIParag = []
     for(let i=0; i<4; i++){
-        contentItemParagraphs.push(document.createElement("p"));
+        contentIParag.push(document.createElement("p"));
     }
     
-    contentItemParagraphs[0].innerText = `Title: ${movie.movie_title}`
-    contentItemParagraphs[1].innerText = `Genres: ${movie.movie_genres}`
-    contentItemParagraphs[2].innerText = `Agender: ${movie.agender}`
-    contentItemParagraphs[3].innerText = `Company: ${movie.stock_name}`
+    contentIParag[0].innerText = `Title: ${movie.movie_title}`;
+    contentIParag[1].innerText = `Genres: ${movie.movie_genres}`;
+    contentIParag[2].innerText = `Agender: ${movie.date}`;
+    contentIParag[3].innerText = `Company: ${movie.company_name}`;
+    
+    contentItemDiv.setAttribute("class", "content-info-div")
+    contentItemDivButtons.setAttribute("class", "content-buttons")
+
+    updateButton.innerText = "Update";
+    updateButton.setAttribute("href", `http://127.0.0.1:8000/movies/${movie.id}`);
+    updateButton.setAttribute("class", "update-button");
+    
+    deleteButton.innerHTML = "Delete";
+    deleteButton.setAttribute("href", `http://127.0.0.1:8000/movies/${movie.id}`);
+    deleteButton.setAttribute("class", "delete-button");
 
     for(let i=0; i<4; i++){
-        contentItem.appendChild(contentItemParagraphs[i])   
+        contentItemDiv.appendChild(contentIParag[i]);
     }
 
-    let horizontalLine = document.createElement("hr");
-    contentItem.appendChild(horizontalLine);
+    contentItemDivButtons.appendChild(updateButton);
+    contentItemDivButtons.appendChild(deleteButton);
+
+    contentItem.appendChild(contentItemDiv);
+    contentItem.appendChild(contentItemDivButtons);
+
 
     context.appendChild(contentItem);
 }
