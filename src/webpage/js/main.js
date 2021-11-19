bc012cadf6f57a7a2c59cdb66d24a00d04685b70
Luis Quinones <lpaandres2020@gmail.com>
2021-11-18 20:53:01 -0500

    Summary: Now really clean. Mod: JS(Refactorized(let => const, variable names, function names, restructuration)).

--- src/webpage/js/main.js
+++ src/webpage/js/main.js
@@ -2,194 +2,189 @@
 
 window.onload = function() {
     // Father of everything
-    const content = document.getElementById('content');
+    const CONTENT = document.getElementById('content');
 
 
     fetch('http://127.0.0.1:8000/movies', {})
     .then(response => response.json())
     .then(data => {
-        data.forEach(movie => appendContentToHome(movie, content));
+        data.forEach(movie => appendContentItems(movie, CONTENT));
     });
 }
 
 
-function appendContentToHome(movie, context) {
-    let homepage = document.getElementById("home");
-    homepage.onclick  = function(e) {
+function appendContentItems(movies, context) {
+    const HOMELINK = document.getElementById("home");
+    HOMELINK.onclick  = function(e) {
         e.preventDefault();
         location.reload();
     }
     
-    let addbutton = document.getElementById("add-content");
-    let updateButton = document.createElement("a");
-    let deleteButton = document.createElement("a");
+    const ADDBUTTON = document.getElementById("add-content");
+    const UPDATEBUTTON = document.createElement("a");
+    const DELETEBUTTON = document.createElement("a");
     
-    let contentIParag = []
+    const CONTENTITEM_DIV_PARAGRAPH = []
     for(let i=0; i<4; i++){
-        contentIParag.push(document.createElement("p"));
+        CONTENTITEM_DIV_PARAGRAPH.push(document.createElement("p"));
     }
     
-    // Data representation
-    let genresText = "";
-    movie.movie_genres.indexOf("|") ? movie.movie_genres.split("|").forEach(gender => {genresText = `${genresText} ${gender}, `}) : movie.movie_genres;
-    
-    contentIParag[0].innerText = `Title: ${movie.movie_title}`;
-    contentIParag[1].innerText = `Genres: ${genresText.substring(0, genresText.length - 2)}.`;
-    contentIParag[2].innerText = `Agender: ${movie.date}`;
-    contentIParag[3].innerText = `Company: ${movie.company_name}`;
+    CONTENTITEM_DIV_PARAGRAPH[0].innerText = `Title: ${movies.movie_title}`;
+    CONTENTITEM_DIV_PARAGRAPH[1].innerText = `Genres: ${embellishGenresText(movies.movie_genres)}.`;
+    CONTENTITEM_DIV_PARAGRAPH[2].innerText = `Agender: ${movies.date}`;
+    CONTENTITEM_DIV_PARAGRAPH[3].innerText = `Company: ${movies.company_name}`;
     
     
     // HTTP Methods
-    addbutton.onclick = function(e) {
+    ADDBUTTON.onclick = function(e) {
         e.preventDefault();
         createFormulary(undefined, context, 'POST');
     };
     
     
-    updateButton.innerText = "Update";
-    updateButton.setAttribute("href", `http://127.0.0.1:8000/movies/${movie.id}`);
-    updateButton.setAttribute("class", "update-button");
-    updateButton.onclick = function(e){
+    UPDATEBUTTON.innerText = "Update";
+    UPDATEBUTTON.setAttribute("href", `http://127.0.0.1:8000/movies/${movies.id}`);
+    UPDATEBUTTON.setAttribute("class", "update-button");
+    UPDATEBUTTON.onclick = function(e){
         e.preventDefault();
         
         // To fill the form
-        fetch(`http://127.0.0.1:8000/movies/${movie.id}`, {})
+        fetch(`http://127.0.0.1:8000/movies/${movies.id}`, {})
         .then(response => response.json())
-        .then(movie_data => createFormulary(movie_data, context, 'PUT'));
+        .then(movieData => createFormulary(movieData, context, 'PUT'));
     }
     
-    deleteButton.innerHTML = "Delete";
-    deleteButton.setAttribute("href", `http://127.0.0.1:8000/movies/${movie.id}`);
-    deleteButton.setAttribute("class", "delete-button");
-    deleteButton.onclick = function (e) {
+    DELETEBUTTON.innerHTML = "Delete";
+    DELETEBUTTON.setAttribute("href", `http://127.0.0.1:8000/movies/${movies.id}`);
+    DELETEBUTTON.setAttribute("class", "delete-button");
+    DELETEBUTTON.onclick = function (e) {
         e.preventDefault();
         
-        return fetch(`http://127.0.0.1:8000/movies/${movie.id}`, {
+        return fetch(`http://127.0.0.1:8000/movies/${movies.id}`, {
             method: 'DELETE'
         })
         .then((response) => response.json())
         .then(function() {
-            let childToDelete = document.getElementById(`${movie.id}`);
+            let childToDelete = document.getElementById(`${movies.id}`);
             context.removeChild(childToDelete);
         });
     }
 
     // Showing data in home
-    let contentItem = document.createElement("div");
-    contentItem.setAttribute("class", "content-item");
-    contentItem.setAttribute("id", `${movie.id}`);
+    const CONTENTITEM = document.createElement("div");
+    CONTENTITEM.setAttribute("class", "content-item");
+    CONTENTITEM.setAttribute("id", `${movies.id}`);
 
-    let contentItemDiv = document.createElement("div");
-    contentItemDiv.setAttribute("class", "content-info-div")
+    const CONTENTITEM_DIV = document.createElement("div");
+    CONTENTITEM_DIV.setAttribute("class", "content-info-div")
 
-    let contentItemDivButtons = document.createElement("div");
+    const CONTENTITEM_DIVBUTTONS = document.createElement("div");
     
-    contentItemDivButtons.setAttribute("class", "content-buttons")
-    contentItemDivButtons.appendChild(updateButton);
-    contentItemDivButtons.appendChild(deleteButton);
+    CONTENTITEM_DIVBUTTONS.setAttribute("class", "content-buttons")
+    CONTENTITEM_DIVBUTTONS.appendChild(UPDATEBUTTON);
+    CONTENTITEM_DIVBUTTONS.appendChild(DELETEBUTTON);
     
     for(let i=0; i<4; i++){
-        contentItemDiv.appendChild(contentIParag[i]);
+        CONTENTITEM_DIV.appendChild(CONTENTITEM_DIV_PARAGRAPH[i]);
     }
     
-    contentItem.appendChild(contentItemDiv);
-    contentItem.appendChild(contentItemDivButtons);
+    CONTENTITEM.appendChild(CONTENTITEM_DIV);
+    CONTENTITEM.appendChild(CONTENTITEM_DIVBUTTONS);
     
     
-    context.appendChild(contentItem);
+    context.appendChild(CONTENTITEM);
 }
 
-function createFormulary(movie_data, context, method) {
+function createFormulary(movieData, context, method) {
     // Changing interface
     context.innerHTML = '';
-    let formulary = document.createElement("form");
-    let formularyDiv = document.createElement("div");
-    let label_m_title = document.createElement("label");
-    label_m_title.setAttribute("for", "input-m-title");
-    label_m_title.innerText = 'Movie title';
+    const FORMULARY = document.createElement("form");
+    const FORMULARY_DIV = document.createElement("div");
+    FORMULARY_DIV.classList = "formulary-content";
+    const LABEL_TITLE = document.createElement("label");
+    LABEL_TITLE.setAttribute("for", "input-m-title");
+    LABEL_TITLE.innerText = 'Movie title';
 
-    let input_m_title = document.createElement("input");
-    input_m_title.setAttribute("id", "input-m-title");
-    input_m_title.setAttribute("placeholder", "Enter a title here");
-    input_m_title.setAttribute("type", "text");
-    input_m_title.setAttribute("name", "title");
+    const INPUT_TITLE = document.createElement("input");
+    INPUT_TITLE.setAttribute("id", "input-m-title");
+    INPUT_TITLE.setAttribute("placeholder", "Enter a title here");
+    INPUT_TITLE.setAttribute("type", "text");
+    INPUT_TITLE.setAttribute("name", "title");
     
-    let label_m_genres = document.createElement("label");
-    label_m_genres.setAttribute("for", "input-m-genres");
-    label_m_genres.innerText = 'Movie genres';
+    const LABEL_GENRES = document.createElement("label");
+    LABEL_GENRES.setAttribute("for", "input-m-genres");
+    LABEL_GENRES.innerText = 'Movie genres';
     
-    let input_m_genres = document.createElement("input");
-    input_m_genres.setAttribute("id", "input-m-genres");
-    input_m_genres.setAttribute("placeholder", "Enter the genres here");
-    input_m_genres.setAttribute("type", "text");
-    input_m_genres.setAttribute("name", "genres");
+    const INPUT_GENRES = document.createElement("input");
+    INPUT_GENRES.setAttribute("id", "input-m-genres");
+    INPUT_GENRES.setAttribute("placeholder", "Enter the genres here");
+    INPUT_GENRES.setAttribute("type", "text");
+    INPUT_GENRES.setAttribute("name", "genres");
 
-    let label_m_date = document.createElement("label");
-    label_m_date.setAttribute("for", "input-m-title");
-    label_m_date.innerText = "Date";
+    const LABEL_DATE = document.createElement("label");
+    LABEL_DATE.setAttribute("for", "input-m-title");
+    LABEL_DATE.innerText = "Date";
 
-    let input_m_date = document.createElement("input");
-    input_m_date.setAttribute("id", "input-m-title");
-    input_m_date.setAttribute("placeholder", "Insert date here, format(DD/M/YYYY)");
-    input_m_date.setAttribute("type", "date");
-    input_m_date.setAttribute("name", "date");
+    const INPUT_DATE = document.createElement("input");
+    INPUT_DATE.setAttribute("id", "input-m-title");
+    INPUT_DATE.setAttribute("placeholder", "Insert date here, format(DD/M/YYYY)");
+    INPUT_DATE.setAttribute("type", "date");
+    INPUT_DATE.setAttribute("name", "date");
 
-    let label_m_company = document.createElement("label");
-    label_m_company.setAttribute("for", "input-m-company");
-    label_m_company.innerText = "Company name";
+    const LABEL_COMPANY = document.createElement("label");
+    LABEL_COMPANY.setAttribute("for", "input-m-company");
+    LABEL_COMPANY.innerText = "Company name";
 
-    let input_m_company = document.createElement("input");
-    input_m_company.setAttribute("id", "input-m-company");
-    input_m_company.setAttribute("placeholder", "Insert company name here");
-    input_m_company.setAttribute("type", "text");
-    input_m_company.setAttribute("name", "company");
+    const INPUT_COMPANY = document.createElement("input");
+    INPUT_COMPANY.setAttribute("id", "input-m-company");
+    INPUT_COMPANY.setAttribute("placeholder", "Insert company name here");
+    INPUT_COMPANY.setAttribute("type", "text");
+    INPUT_COMPANY.setAttribute("name", "company");
 
-    let input_m_send = document.createElement("input");
-    input_m_send.setAttribute("type", "submit");
-    input_m_send.setAttribute("value", "Send");
-    input_m_send.setAttribute("id", "submit-send")
+    const INPUT_SUBMIT = document.createElement("input");
+    INPUT_SUBMIT.setAttribute("type", "submit");
+    INPUT_SUBMIT.setAttribute("value", "Send");
+    INPUT_SUBMIT.setAttribute("id", "submit-send")
 
     if (method === 'PUT') {
-        input_m_title.setAttribute("value", `${movie_data.movie_title}`)
-        input_m_genres.setAttribute("value", `${movie_data.movie_genres}`)
-        input_m_date.setAttribute("value", `${movie_data.date}`)
-        input_m_company.setAttribute("value", `${movie_data.company_name}`)
+        INPUT_TITLE.setAttribute("value", `${movieData.movie_title}`)
+        INPUT_GENRES.setAttribute("value", `${movieData.movie_genres}`)
+        INPUT_DATE.setAttribute("value", `${movieData.date}`)
+        INPUT_COMPANY.setAttribute("value", `${movieData.company_name}`)
 
     } else if (method === 'POST') {
-        formulary.setAttribute("action", `http://127.0.0.1:8000/movies`);
-        formulary.setAttribute("method", "post");
+        FORMULARY.setAttribute("action", `http://127.0.0.1:8000/movies`);
+        FORMULARY.setAttribute("method", "post");
     }
 
-    formularyDiv.appendChild(label_m_title);
-    formularyDiv.appendChild(input_m_title);
-    formularyDiv.appendChild(label_m_genres);
-    formularyDiv.appendChild(input_m_genres);
-    formularyDiv.appendChild(label_m_date);
-    formularyDiv.appendChild(input_m_date);
-    formularyDiv.appendChild(label_m_company);
-    formularyDiv.appendChild(input_m_company);
-    formularyDiv.appendChild(input_m_send);
+    FORMULARY_DIV.appendChild(LABEL_TITLE);
+    FORMULARY_DIV.appendChild(INPUT_TITLE);
+    FORMULARY_DIV.appendChild(LABEL_GENRES);
+    FORMULARY_DIV.appendChild(INPUT_GENRES);
+    FORMULARY_DIV.appendChild(LABEL_DATE);
+    FORMULARY_DIV.appendChild(INPUT_DATE);
+    FORMULARY_DIV.appendChild(LABEL_COMPANY);
+    FORMULARY_DIV.appendChild(INPUT_COMPANY);
+    FORMULARY_DIV.appendChild(INPUT_SUBMIT);
 
-    formularyDiv.classList = "formulary-content";
-
-    formulary.appendChild(formularyDiv);
-    context.appendChild(formulary);
+    FORMULARY.appendChild(FORMULARY_DIV);
+    context.appendChild(FORMULARY);
     
     // REQUEST TO API
     if (method === 'PUT') {
-        input_m_send.onclick = function(e) {
+        INPUT_SUBMIT.onclick = function(e) {
             e.preventDefault();
     
-            fetch(`http://127.0.0.1:8000/movies/${movie_data.id}`, {
+            fetch(`http://127.0.0.1:8000/movies/${movieData.id}`, {
                 headers: {
                     'Content-Type': 'application/json'
                 },
                 method: 'PUT',
                 body: JSON.stringify({
-                    'movie_title': analyzingData(input_m_title.value),
-                    'movie_genres': analyzingData(input_m_genres.value),
-                    'date': input_m_date.value,
-                    'company_name': analyzingData(input_m_company.value)
+                    'movie_title': transformRequests(INPUT_TITLE.value),
+                    'movie_genres': transformRequests(INPUT_GENRES.value),
+                    'date': INPUT_DATE.value,
+                    'company_name': transformRequests(INPUT_COMPANY.value)
                 })
             }).then(res => res.json())
             .catch(error => console.error(`Error ${error}.`))
@@ -197,7 +192,7 @@ function createFormulary(movie_data, context, method) {
             .then(location.reload());
         }   
     } else if (method === 'POST') {
-        input_m_send.onclick = function(e) {
+        INPUT_SUBMIT.onclick = function(e) {
             e.preventDefault();
 
             fetch(`http://127.0.0.1:8000/movies`, {
@@ -206,10 +201,10 @@ function createFormulary(movie_data, context, method) {
                 },
                 method: 'POST',
                 body: JSON.stringify({
-                    'movie_title': analyzingData(input_m_title.value),
-                    'movie_genres': analyzingData(input_m_genres.value),
-                    'date': input_m_date.value,
-                    'company_name': analyzingData(input_m_company.value)
+                    'movie_title': transformRequests(INPUT_TITLE.value),
+                    'movie_genres': transformRequests(INPUT_GENRES.value),
+                    'date': INPUT_DATE.value,
+                    'company_name': transformRequests(INPUT_COMPANY.value)
                 })
             }).then(res => res.json())
             .catch(error => console.error(`Error ${error}.`))
@@ -219,10 +214,18 @@ function createFormulary(movie_data, context, method) {
     }
 }
 
-function analyzingData(data){
+function transformRequests(data){
     // Validates if the string has a simple quote, if this don't be fixed, we would have a query problem
-    // Modify the string if necessary
+    // This modify the string if is necessary
     if (data.indexOf("'")) {
         return `${data.substring(0, data.indexOf("'"))}'${data.substring(data.indexOf("'"), data.length)}'`;
     }
+}
+
+function embellishGenresText(text) {
+    let genresText = "";
+    text.indexOf("|") ? text.split("|").forEach(gender => {genresText = `${genresText} ${gender}, `}) : text;
+    genresText = genresText.substring(0, genresText.length - 2);
+    
+    return genresText;
 }
